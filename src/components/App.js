3c9536d1ef608984114f57ba2a3e7ab76ab22d73
Jacob <yawjacob@yahoo.com>
2022-01-17 21:13:00 -0600

    commit tree 4a4c973577b504dbe74dd50ac8558a5cf9539f02

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import HogTiles from "./HogTiles";
 
 function App() {
+
+	console.log(hogs)
+
 	return (
 		<div className="App">
 			<Nav />
+			<HogTiles hogs={hogs}/>
 		</div>
 	);
 }
